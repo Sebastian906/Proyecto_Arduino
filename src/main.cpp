29b940367557b026e88696fd366fbe4da7304370
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-04-09 17:35:28 -0500

    Corrección en platformio.ini y prueba del primer código

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(19200,SERIAL_8N1);
+  Serial1.begin(9600,SERIAL_5N1);
+  Serial2.begin(1200);
+
+  Serial.write('H');
+  Serial.write('o');
+  Serial.write('l');
+  Serial.write('a');
+
+  Serial.println("Hola");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
